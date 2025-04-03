3c30966264fdd69e944932f7624c617640c14357
10BS <reue5118@protonmail.com>
2025-04-03 10:54:51 +1000

    Optimized list_exts()

--- main.py
+++ main.py
@@ -1,37 +1,29 @@
 import os
 import platform
 import shutil
-
+from os import DirEntry
 
 if platform.system == "Windows":
     from colorama import just_fix_windows_console
     just_fix_windows_console()
 
 
-def list_exts(directory: str) -> set:
-    exts = set()
-    for p in os.scandir(directory):
-        if p.is_file():
-            root, ext = os.path.splitext(p)
-            exts.add(ext[1:]) if ext else ""
-    return exts
-
-
-def mk_mv2dir(source, destination) -> None:
-    os.makedirs(destination, exist_ok=True)
-    shutil.move(source, destination)
-
-
-def mv(where: str) -> None:
-    for path in os.scandir(where):
-        if path.is_file():
-            root, ext = os.path.splitext(path)
-            if ext[1:]:
-                destination = os.path.join(where, ext[1:].upper())
-                mk_mv2dir(path, destination)
-                print(f"{path.path} moved to {destination}")
+def sort_entries(where: str) -> None:
+    for entry in os.scandir(where):
+        if entry.is_file():
+            name, _ext = os.path.splitext(entry)
+            ext = _ext[1:]
+            exec_path = os.path.join(where, os.path.basename(__file__))
+            if ext:
+                if entry.path == exec_path:
+                    continue
+                elif entry.path != exec_path:
+                    destination = os.path.join(where, ext.upper())
+                    os.makedirs(destination, exist_ok=True)
+                    shutil.move(entry, destination)
+                    print(f"{entry.path} moved to {destination}")
 
 
 if __name__ == "__main__":
-    path = input("Enter the path: ")
-    mv(path)
+    where = input("Enter the path: ")
+    sort_entries(where)
