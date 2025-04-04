12f780e09920e27ca2308cef45aa4d59aa86ddbe
10BS <reue5118@protonmail.com>
2025-04-04 13:12:51 +1000

    Complete x2

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import platform
 import shutil
-from os import DirEntry
 
 if platform.system == "Windows":
     from colorama import just_fix_windows_console
@@ -11,19 +10,22 @@ if platform.system == "Windows":
 def sort_entries(where: str) -> None:
     for entry in os.scandir(where):
         if entry.is_file():
-            name, _ext = os.path.splitext(entry)
-            ext = _ext[1:]
-            exec_path = os.path.join(where, os.path.basename(__file__))
+            name, e = os.path.splitext(entry)
+            ext: str = e[1:]
+            exec_path: str = os.path.join(where, os.path.basename(__file__))
             if ext:
                 if entry.path == exec_path:
                     continue
                 elif entry.path != exec_path:
-                    destination = os.path.join(where, ext.upper())
+                    destination: str = os.path.join(where, ext.upper())
                     os.makedirs(destination, exist_ok=True)
-                    shutil.move(entry, destination)
-                    print(f"{entry.path} moved to {destination}")
+                    try:
+                        shutil.move(entry, destination)
+                        print(f"{entry.path} moved to {destination}")
+                    finally:
+                        continue
 
 
 if __name__ == "__main__":
-    where = input("Enter the path: ")
-    sort_entries(where)
+    where_sort: str = input("Enter the path: ")
+    sort_entries(where_sort)
