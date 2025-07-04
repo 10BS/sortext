91fee8b395a1912d5cc0c4e9763948672861f7ee
10BS <reue5118@protonmail.com>
2025-07-04 18:27:39 +1000

    Cleared

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-import sort
+from listing import Listing
+from operator import Operator
 import os, shutil
 
 
 if __name__ == "__main__":
-    path = r".fleet"
-    filtered = sort.filter_by_ext(path)
-    for e in filtered:
-        dst = os.path.join(path, e["ext"].upper())
-        src = e["path"]
-        os.makedirs(dst, exist_ok=True)
-        shutil.move(src, dst)
-    print("\nProcess finished with exit code 0")
+    pass
